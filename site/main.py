2bfb0d742e47cd83eb951eb66c1acf49c080e492
katysidorova <Katy.sidorova.2008@yandex.ru>
2024-02-14 18:58:45 +0400

    2 задание

--- site/main.py
+++ site/main.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
-def index1():
-    return render_template('index.html', title='Домашняя страница')
+@app.route('/index/<title>')
+def index1(title):
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
 
 
 if __name__ == "__main__":
